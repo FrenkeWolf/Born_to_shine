a4686f1652bb6ab51cf881777e15a69d23af01c7
Frenke_Wolf <dmitryrukoit@mail.ru>
2024-02-07 22:13:59 +0300

    Версия 2

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "<p>Born to shine</p>"
+
+
+@app.route('/about')
+def about():
+    return "<p>о нас</p>"
+#kkl;k
+if __name__ == '__main__':
+    app.run(debug=True)
